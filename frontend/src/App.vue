30e4764de40aef802ab0f74b9cdfc2b8467429c3
akshaydotweb <akshayvmudaliar@gmail.com>
2026-03-29 14:10:23 +0530

    updated app.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,19 +2,18 @@
 import { ref } from "vue";
 import Employee from "./pages/Employee.vue";
 import Manager from "./pages/Manager.vue";
-import Admin from "./pages/Admin.vue";
 
 const page = ref("employee");
 </script>
 
 <template>
   <div>
+    <h1>Reimbursement System</h1>
+
     <button @click="page='employee'">Employee</button>
     <button @click="page='manager'">Manager</button>
-    <button @click="page='admin'">Admin</button>
 
     <Employee v-if="page==='employee'" />
     <Manager v-if="page==='manager'" />
-    <Admin v-if="page==='admin'" />
   </div>
 </template>
