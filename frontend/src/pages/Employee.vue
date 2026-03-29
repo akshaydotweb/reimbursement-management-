0d783ee492531ca2c7748427bf82511e0fdeddcb
akshaydotweb <akshayvmudaliar@gmail.com>
2026-03-29 14:48:43 +0530

    list reloads instatnly after submit

--- frontend/src/pages/Employee.vue
+++ frontend/src/pages/Employee.vue
@@ -1,12 +1,18 @@
 <script setup>
 import ExpenseForm from "../components/ExpenseForm.vue";
 import ExpenseList from "../components/ExpenseList.vue";
+import { ref } from "vue";
+
+const refreshKey = ref(0);
+
+const refreshList = () => {
+  refreshKey.value++;
+};
 </script>
 
 <template>
   <div>
-    <h2>Employee Dashboard</h2>
-    <ExpenseForm />
-    <ExpenseList />
+    <ExpenseForm @refresh="refreshList" />
+    <ExpenseList :key="refreshKey" />
   </div>
 </template>
